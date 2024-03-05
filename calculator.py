955f1392a352046c583b18bbc0450055ccad7272
Samadkhan7860 <159469829+Samadkhan7860@users.noreply.github.com>
2024-03-06 01:33:52 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,24 @@
+print ("Calculator")
+
+num1 = float(input("Enter first number here:"))
+
+num2 = float(input("Enter second number here:"))
+
+print("press 1 for addition \npress 2 for substraction \npress 3 for multiplications\npress 4 for division")
+
+choice = int(input("enter your choice from 1-4;"))
+
+if choice == 1:
+    print ("The addition of given two numbers is",num1 + num2);
+
+elif choice == 2:
+    print ("The addition of given two numbers is",num1 - num2);
+
+elif choice == 3:
+    print ("The addition of given two numbers is",num1 * num2);
+
+elif choice == 4:
+    print ("The addition of given two numbers is",num1 / num2);
+
+else:
+    print("Invalid input");
